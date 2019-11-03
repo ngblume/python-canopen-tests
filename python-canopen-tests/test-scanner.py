2abe2d44a906c55c9d61768265f16db29c48b6bf
Niels Göran Blume <ngblume@gmail.com>
2019-11-03 14:41:28 +0100

    Switched to using existing bus (NOT WORKING YET)

--- python-canopen-tests/test-scanner.py
+++ python-canopen-tests/test-scanner.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-"""
-This example scans nodes 1 to 127 by trying to perform an SDO operation
-"""
-
 import canopen
 import can
 import time
@@ -15,14 +11,23 @@ import os
 EDS_PATH = os.path.join(os.path.dirname(__file__), '../00_EDS/SimNode/SimNode.eds')
 
 # Set logging level
-# logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
+
+# =====================================================================================================================================
+# Create bus (using SocketCAN, can0 and a bit rate of 250 kB/s)
+can_bus = can.interface.Bus(bustype='socketcan', channel='can0', bitrate=250000)
+# can_bus = can.interface.Bus(bustype='pcan', channel='PCAN_USBBUS1', bitrate=250000)
 
 # =====================================================================================================================================
 # Create network_real
 network_real = canopen.Network()
 
-# Connect network_real to specific CAN bus
-network_real.connect(channel='can0', bustype='socketcan')
+# Assign network to previoulyly created bus
+network_real.bus = can_bus
+
+# Connect network_real to specific CAN bus 
+# > notnecessary anymore, since bus instance can_bus is used
+# network_real.connect(channel='can0', bustype='socketcan')
 
 # Add simulated note to network_real
 remoteSimNode_0x05 = network_real.add_node(0x05, EDS_PATH)
@@ -32,12 +37,22 @@ remoteSimNode_0x10 = network_real.add_node(0x10, EDS_PATH)
 # Create network_sim = network used to CREATE simulated nodes
 network_sim = canopen.Network()
 
+# Assign network to bus
+network_sim.bus = can_bus
+
 # Connect network_sim to same CAN bus
-network_sim.connect(channel='can0', bustype='socketcan')
+# > notnecessary anymore, since bus instance can_bus is used
+# network_sim.connect(channel='can0', bustype='socketcan')
 
 # Create simulated nodes (via network_sim) > called "local"
 localSimNode_0x05 = network_sim.create_node(0x05, EDS_PATH)
 localSimNode_0x10 = network_sim.create_node(0x10, EDS_PATH)
+
+# =====================================================================================================================================
+# Add network listeners to created bus
+bus_listeners = [can.Printer()] + network_real.listeners + network_sim.listeners
+# Start the notifier
+notifier = can.Notifier(can_bus, bus_listeners, 0.5)
 
 # =====================================================================================================================================
 # Set Index 0x1000-Subindex 0x00, which is scanned by scanner.search()
@@ -73,11 +88,11 @@ network_real.scanner.search()
 # Wait for nodes to respond
 time.sleep(1)
 for node_id in network_real.scanner.nodes:
-    print("Found node %d!" % node_id)
+    print('Found node: ', hex(node_id))
 
 # =====================================================================================================================================
 time.sleep(1)
-# Set SDO timeout longer
+# Set SDO timeout longer (as an example)
 remoteSimNode_0x05.sdo.RESPONSE_TIMEOUT = 2.0
 remoteSimNode_0x10.sdo.RESPONSE_TIMEOUT = 2.0
 # SDO upload via expedited transfer
